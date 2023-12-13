be0c1385f32f49f7d9a7519f3a8c3d84d78fabfe
izzarzn <arjunmeega28@gmail.com>
2023-12-13 21:18:10 +0530

    Updated Fitness App
    Updated Node JS

---
+++ model/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userData = new mongoose.Schema({
+    name: String,
+    email: String,
+    mobile: String,
+    message: String
+});
+
+const User = mongoose.model('User', userData);
+
+module.exports = User;
